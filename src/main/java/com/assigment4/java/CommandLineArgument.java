7920ff0bf9a2302d90dd2d3b2277c56c8a3b5f05
nehakhatriya <nehakhatriya@gmail.com>
2020-09-25 18:50:56 +0530

    Assignment 4

---
+++ src/main/java/com/assigment4/java/CommandLineArgument.java
@@ -0,0 +1,11 @@
+package com.assigment4.java;
+
+public class CommandLineArgument {
+   public static void main(String[] args) {
+	   int sum=0;
+	   for(int i=0;i<args.length;i++){
+		   sum+=Integer.parseInt(args[i]);
+	   }
+	   System.out.print(sum);
+   }
+}
